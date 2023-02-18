48e0e97f3db54916dfb102221223be7ac44e2d0a
yashturkar <yashturkar@gmail.com>
2023-02-17 21:29:57 -0500

    damage app created, argeparse added

--- scripts/damage_app.py
+++ scripts/damage_app.py
@@ -2,13 +2,18 @@ import open3d as o3d
 import numpy as np
 import copy
 import os,sys
+import argparse
+
+# python damage_pointcloud.py point_cloud.pcd gaussian --sigma_x 0.05 --sigma_y 0.05 --sigma_z 0.05 --visualize --save --output damaged_point_cloud.pcd
+# python damage_pointcloud.py point_cloud.pcd random --percentage 0.9 --visualize --save --output damaged_point_cloud.pcd
+# python damage_pointcloud.py point_cloud.pcd remove --percentage 0.1 --visualize --save --output damaged_point_cloud.pcd
+# python damage_pointcloud.py point_cloud.pcd voxel --voxel_size 3 --visualize --save --output damaged_point_cloud.pcd
+# python damage_pointcloud.py point_cloud.pcd corners --corner top_left --visualize --save --output damaged_point_cloud.pcd
 
 def load_point_cloud(filename):
     pcd = o3d.io.read_point_cloud(filename)
     return pcd
 
-
-# Function to remove points from a point cloud from a corner
 def deleteCorners(point_cloud, corner):
     """
     Effect on completeness
@@ -57,8 +62,6 @@ def deleteCorners(point_cloud, corner):
     return point_cloud
 
 
-
-# TODO Check
 def addRandomPoints(point_cloud, percentage):
     """
     Effect on artifacts
@@ -91,7 +94,6 @@ def addGaussian(pcd,sigma_x=0.01,sigma_y=0.01,sigma_z=0.01):
     return noisy_pcd
 
 
-# TODO Check
 def removeRandomPoints(point_cloud, percentage):
     """
     Effect on completeness
@@ -114,29 +116,6 @@ def removeRandomPoints(point_cloud, percentage):
     
     # Remove the selected points from the existing point cloud
     point_cloud.points = o3d.utility.Vector3dVector(np.delete(point_cloud.points, indices, axis=0))
-
-    return point_cloud
-
-
-# TODO Check
-# FIXME Does something weird
-def uniformDownsample(point_cloud, percentage):
-    """
-    Effect on resolution
-    Downsamples a point cloud by a specified percentage using uniform sampling.
-    
-    Parameters:
-    point_cloud (open3d.geometry.PointCloud): the point cloud object to downsample
-    percentage (float): the percentage of points to keep (between 0 and 1)
-    
-    Returns:
-    point_cloud (open3d.geometry.PointCloud): the downsampled point cloud object
-    """
-    # Determine the number of points to keep based on the percentage and the size of the existing point cloud
-    num_points = int(len(point_cloud.points) * percentage)
-    
-    # Downsample the point cloud using uniform sampling
-    point_cloud = point_cloud.uniform_down_sample( len(point_cloud.points)/ num_points)
 
     return point_cloud
 
@@ -179,27 +158,41 @@ def savePointcloud(pcd, filename):
     # Save the point cloud
     o3d.io.write_point_cloud(filename, pcd)
 
-
-
 def main():
+    parser = argparse.ArgumentParser(description='Damage point cloud')
+    parser.add_argument('pcd', type=str, help='Path to point cloud')
+    parser.add_argument('damage', type=str, help='Type of damage to apply')
+    parser.add_argument('--sigma_x', type=float, help='Standard deviation of gaussian noise in x direction')
+    parser.add_argument('--sigma_y', type=float, help='Standard deviation of gaussian noise in y direction')
+    parser.add_argument('--sigma_z', type=float, help='Standard deviation of gaussian noise in z direction')
+    parser.add_argument('--percentage', type=float, help='Percentage of points to remove')
+    parser.add_argument('--corner', type=str, help='Corner to remove points from')
+    parser.add_argument('--voxel_size', type=float, help='Size of voxel to use for downsampling')
+    parser.add_argument('--visualize', action='store_true', help='Visualize point cloud')
+    parser.add_argument('--save', action='store_true', help='Save point cloud')
+    parser.add_argument('--output', type=str, help='Path to save point cloud')
+    args = parser.parse_args()
+
     # Load point cloud
-    pcd = load_point_cloud(sys.argv[1])
+    pcd = load_point_cloud(args.pcd)
 
     # Damage point cloud
-    # damaged_pcd = damage_point_cloud(pcd, sigma_x=0.05, sigma_y=0.05, sigma_z=0.05, percentage=50)
+    if args.damage == "gaussian":
+        damage_pcd = addGaussian(pcd, args.sigma_x, args.sigma_y, args.sigma_z)
+    elif args.damage == "random":
+        damage_pcd = addRandomPoints(pcd, args.percentage)
+    elif args.damage == "remove":
+        damage_pcd = removeRandomPoints(pcd, args.percentage)
+    elif args.damage == "voxel":
+        damage_pcd = voxelDownsample(pcd, args.voxel_size)
+    elif args.damage == "corners":
+        damage_pcd = deleteCorners(pcd, args.corner)
 
-    # damage_pcd = removeRandomPoints(pcd, 0.9) # Works -> Completeness
-    # damage_pcd = uniformDownsample(pcd, 0.1)
-    # damage_pcd = addGaussian(pcd, 0.1, 0.1, 0.1) # Works -> Accuracy
-    # damage_pcd = addRandomPoints(pcd, 0.1) # Works -> Artifacts
-    # damage_pcd = deleteCorners(pcd, corner="top_left") # Works -> Completeness
-    damage_pcd = voxelDownsample(pcd, 3) # Works -> Resolution
+    if args.visualize:
+        visualizePointcloud(damage_pcd)
 
-    # savePointcloud(damaged_pcd, sys.argv[2])
-
-    # Visualize the point cloud
-    visualizePointcloud(damage_pcd)
-    # visualizePointcloudSeparate(pcd, damage_pcd)
+    if args.save:
+        savePointcloud(damage_pcd, args.output)
 
 if __name__ == "__main__":
     main()
